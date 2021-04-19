c8ae3b24e284e19386e10869671bee50c50dd854
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2021-04-20 09:11:56 +1000

    finished backend

--- file_renamer.py
+++ file_renamer.py
@@ -1,5 +1,6 @@
 from zipfile import ZipFile
 import os
+import shutil
 
 def unzip(file,dest):
     try:
@@ -8,14 +9,29 @@ def unzip(file,dest):
     except:
         print("Zip file error")
 
-def rename():
-    pass
+def rename_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for file_name in files:
+            new_name = file_name[9:]
+            os.rename(f'{directory}/{file_name}',f'{directory}/{new_name}')
 
-
+def add_file(file,directory):
+    for root, dirs, files in os.walk(directory):
+        dot = file.find(".")
+        new_ext = file[dot:]
+        
+        for filename in files:
+            dot = filename.find("-")
+            new_filename = f'{filename[:dot]} Criteria{new_ext}'
+            shutil.copyfile(file,f'{directory}/{new_filename}')
+            
 
 
 # ----- MAIN PROGRAM -----
 zipped_file = '11.FIA1___Creating_with_Code___Investigation_Final.zip'
 destination = 'student_files'
+criteria = 'criteria.pdf'
 
 unzip(zipped_file, destination)
+rename_files(destination)
+add_file(criteria,destination)
