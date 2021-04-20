1412d48a58a278d79037e652c553816fc88331d8
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2021-04-20 16:12:49 +1000

    finished GUI

--- file_renamer.py
+++ file_renamer.py
@@ -1,6 +1,9 @@
 from zipfile import ZipFile
 import os
 import shutil
+import tkinter as tk
+from tkinter import filedialog
+
 
 def unzip(file,dest):
     try:
@@ -24,14 +27,55 @@ def add_file(file,directory):
             dot = filename.find("-")
             new_filename = f'{filename[:dot]} Criteria{new_ext}'
             shutil.copyfile(file,f'{directory}/{new_filename}')
-            
 
+def zip_btn_click():
+    root.zipped_file = filedialog.askopenfilename(initialdir=".")
+    print(root.zipped_file)
 
-# ----- MAIN PROGRAM -----
-zipped_file = '11.FIA1___Creating_with_Code___Investigation_Final.zip'
-destination = 'student_files'
-criteria = 'criteria.pdf'
+def dest_btn_click():
+    root.destination = filedialog.askdirectory(initialdir=".")
+    print(root.destination)
 
-unzip(zipped_file, destination)
-rename_files(destination)
-add_file(criteria,destination)
+def crit_btn_click():
+    root.criteria = filedialog.askopenfilename(initialdir=".")
+    print(root.criteria)
+
+def go_btn_click():
+    unzip(root.zipped_file, root.destination)
+    rename_files(root.destination)
+    add_file(root.criteria,root.destination)
+
+# ***** MAIN PROGRAM *****
+
+# create window
+root = tk.Tk()
+root.geometry("800x600")
+root.title("File Renamer")
+
+# *** create entry screen ***
+# zipped file picker
+tk.Label(root, text="Zipped file location").grid(row=1, column=0)
+zip_btn = tk.Button(root, text="Choose File", command=zip_btn_click)
+zip_btn.grid(row=1,column=1)
+
+# desitnation folder
+tk.Label(root, text="Destination folder").grid(row=2, column=0)
+dest_btn = tk.Button(root, text="Choose Folder", command=dest_btn_click)
+dest_btn.grid(row=2,column=1)
+
+# criteria sheet
+tk.Label(root, text="Criteria Sheet").grid(row=3, column=0)
+crit_btn = tk.Button(root, text="Choose File", command=crit_btn_click)
+crit_btn.grid(row=3,column=1)
+
+# go button
+go_btn = tk.Button(root, text="Go", command=go_btn_click)
+go_btn.grid(row=4,column=1)
+
+# global variables
+root.zipped_file = '11.FIA1___Creating_with_Code___Investigation_Final.zip'
+root.destination = 'student_files'
+root.criteria = 'criteria.pdf'
+
+# run window loop
+root.mainloop()
